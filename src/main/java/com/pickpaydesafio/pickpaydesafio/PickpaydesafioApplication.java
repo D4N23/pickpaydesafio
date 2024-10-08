1145ee9805d9b0c6ff71a1a0f78cd18d66f0c1a8
Danilo <daniilosam23@gmail.com>
2024-10-08 00:14:19 -0300

    commit inicial

---
+++ src/main/java/com/pickpaydesafio/pickpaydesafio/PickpaydesafioApplication.java
@@ -0,0 +1,13 @@
+package com.pickpaydesafio.pickpaydesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickpaydesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickpaydesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickpaydesafio/pickpaydesafio/PickpaydesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickpaydesafio.pickpaydesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickpaydesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
